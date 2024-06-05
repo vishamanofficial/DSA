356318f68cd7bda56f5ba46cceb091827814754d
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-06-05 06:57:48 +0530

    dsa material

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-fn(0)
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end=' ')
+    for j in range(i, n):
+        print("*", end=' ')
+    print(
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-n=5
+n = 5
 
 for i in range(n):
     for j in range(i, n):
         print(" ", end=' ')
     for j in range(i+1):
         print("*", end=' ')
+    for j in range(i):
+        print("*", end=' ')
     print()
+    
