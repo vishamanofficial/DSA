a9f72caaeea3a744b8899928a253b240c2e8f6e3
Aman Vishwakarma <vishamanofficial@gmail.com>
2025-03-07 21:06:09 +0530

    dsa

--- 02 basic maths.py
+++ 02 basic maths.py
@@ -163,24 +163,24 @@
 # else:
 #       print('prime number')
 
-def is_prime(n):
-    # If n is less than 2, it's not a prime number
-    if n <= 1:
-        return False
+# def is_prime(n):
+#     # If n is less than 2, it's not a prime number
+#     if n <= 1:
+#         return False
     
-    # Check for factors from 2 to the square root of n
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:  # If n is divisible by i, it's not a prime number
-            return False
+#     # Check for factors from 2 to the square root of n
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:  # If n is divisible by i, it's not a prime number
+#             return False
     
-    # If no factors are found, n is a prime number
-    return True   
+#     # If no factors are found, n is a prime number
+#     return True   
 
-# Example usage:
-print(is_prime(2))  # Output: True
-print(is_prime(4))  # Output: False
-print(is_prime(17)) # Output: True
-print(is_prime(20)) # Output: False
+# # Example usage:
+# print(is_prime(2))  # Output: True
+# print(is_prime(4))  # Output: False
+# print(is_prime(17)) # Output: True
+# print(is_prime(20)) # Output: False
 
 
 
@@ -199,14 +199,14 @@ print(is_prime(20)) # Output: False
             
 # # GCD to HCF
 
-# a = 52
-# b = 10
-# while (a> 0 and b> 0):
-#      if a>b:
-#           a = a%b
-#      else:
-#       b = b%a
-# if a == 0:
-#     print(b)
-# else:
-#      print(a)
+a = 52
+b = 10
+while (a> 0 and b> 0):
+     if a>b:
+          a = a%b
+     else:
+      b = b%a
+if a == 0:
+    print(b)
+else:
+     print(a)
