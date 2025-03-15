ddc09cdb254502686db4180a447a4b6912b84eb2
Aman Vishwakarma <92659449+vishamanofficial@users.noreply.github.com>
2025-03-15 20:28:45 +0530

    dsa

--- 07 hashing.py
+++ 07 hashing.py
@@ -27,17 +27,17 @@
 
 # Count frequency of each element in the array
 
-def countFreq(arr, n):
-    visited = [False] * n
-    for i in range(n):
-        if (visited[i] == True):
-            continue
-        count = 1
-        for j in range(i + 1, n):
-            if (arr[i] == arr[j]):
-                visited[j] = True
-                count += 1
-        print(arr[i], count)
+# def countFreq(arr, n):
+#     visited = [False] * n
+#     for i in range(n):
+#         if (visited[i] == True):
+#             continue
+#         count = 1
+#         for j in range(i + 1, n):
+#             if (arr[i] == arr[j]):
+#                 visited[j] = True
+#                 count += 1
+#         print(arr[i], count)
 
 
 if __name__ == "__main__":
