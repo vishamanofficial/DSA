f027fcc646f7a803d10e07646bda1e9d039fd656
Aman Vishwakarma <92659449+vishamanofficial@users.noreply.github.com>
2025-03-20 23:03:45 +0530

    dsa

--- 08 selection sort.py
+++ 08 selection sort.py
@@ -4,7 +4,6 @@ def selection (arr1):
             if(arr1[i]>arr1[j]):
                 arr1[i], arr1[j] = arr1[j], arr1[i]
   
-
 arr1 = [10,5,50,99,60,45]
 selection(arr1)
 print('sorted array is:', arr1)
