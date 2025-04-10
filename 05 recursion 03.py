ba0019db79657bddb63b53ac7bc5a0795d851125
Aman Vishwakarma <vishamanofficial@gmail.com>
2025-04-10 09:57:50 +0530

    dsa

--- 05 recursion 03.py
+++ 05 recursion 03.py
@@ -1,15 +1,17 @@
 # sum of first n numbers
 
-print("sum of first n numbers")
-def fn( i , sum):
-    if (i<1):
-        print(sum)
-        return
-    fn(i-1, sum + i)
-fn(10,0)
+# print("sum of first n numbers")
+# def fn( i , sum):
+#     if (i<1):
+#         print(sum)
+#         return
+#     fn(i-1, sum + i)
+# fn(10,0)
 
-def fn1(n):
-    if (n==0):
-        return 0
-    return n + fn1(n-1)
-print(fn1(5))
+# def fn1(n):
+#     if (n==0):
+#         return 0
+#     return n + fn1(n-1)
+# print(fn1(5))
+
+
