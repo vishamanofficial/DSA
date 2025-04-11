72bbedcbfa98d007031880a3629520dc420a811f
Aman Vishwakarma <vishamanofficial@gmail.com>
2025-04-11 10:13:22 +0530

    dsa

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,2 @@
-arr = [1,2,3,1,2,2,1]
-def fn(number):
-    global count 
-    count = 0
-    for i in arr:
-        if (arr[i]==number):
-            count = count + 1
-        return count
-print(count)
-fn(1) 
+n = int(input())
+# arr = list(map(int, input().split()))
